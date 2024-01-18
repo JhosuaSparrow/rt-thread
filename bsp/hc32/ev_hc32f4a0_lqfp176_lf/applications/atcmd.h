4b07780b7c7fcd3e86e90ef7003d1c6477b1b0be
JhosuaSparrow <790041973@qq.com>
2024-01-18 13:13:06 +0800

    update: atcmd.h

--- bsp/hc32/ev_hc32f4a0_lqfp176_lf/applications/atcmd.h
+++ bsp/hc32/ev_hc32f4a0_lqfp176_lf/applications/atcmd.h
@@ -8,7 +8,6 @@ rt_err_t atcmd_client_send(char *cmd, rt_uint8_t client_id, rt_uint8_t recv_line
 rt_err_t atcmd_reset(rt_uint8_t client_id, rt_uint8_t retry_count);
 rt_err_t atcmd_query_imei(rt_uint8_t client_id, rt_uint8_t retry_count);
 rt_err_t atcmd_query_vbat(rt_uint8_t client_id, rt_uint8_t retry_count);
-void atcmd_clients_query_vbat(void);
 at_result_t atcmd_server_find_setup(const char *args);
 void atcmd_server_send_vbat(void *args);
 rt_err_t atcmd_server_send_vbat_running(void);
